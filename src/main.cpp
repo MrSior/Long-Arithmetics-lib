594c344aa602a07dad37fd6cc9a4ae8dc4af5e53
simon <semionchubenko@gmail.com>
2024-02-08 01:59:27 +0300

    add testing pi

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main() {
     // std::cout << (BigNum("0.5") * BigNum("2.0") <= BigNum("2.5"));
 
     auto time_s = high_resolution_clock::now();
-    std::cout << CalcPi(100).toString() << std::endl;
+    std::cout << CalcPi(10).toString() << std::endl;
     auto time_end = high_resolution_clock::now();
     std::cout << "Time: "
               << duration_cast<milliseconds>(time_end - time_s).count()
